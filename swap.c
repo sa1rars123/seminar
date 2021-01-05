a7f27465ac21abf48f8d9e266d0f2579a4ae4439
Saira Rashid <saira.rashid@csu.fullerton.edu>
2021-01-05 14:28:39 -0500

    Added logic to swap two values

--- swap.c
+++ swap.c
@@ -2,13 +2,17 @@
 
 int main(void)
 {
-    int x = 3;
-    int y = 4;
+    int x = 36;
+    int y = 74;
+    int temp = 0; //temp variable for swapping
 
     printf("x is: %i\n", x);
     printf("y is: %i\n", y);
 
     //swapping
+    temp = x;
+    x = y;
+    y = temp;
 
     printf("x is now: %i\n", x);
     printf("y is now: %i\n", y);
