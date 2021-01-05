9fd2243dd269d9f66e12bfbcd8ad232fcfca8232
Saira Rashid <saira.rashid@csu.fullerton.edu>
2021-01-05 13:55:15 -0500

    Added swap.c

---
+++ swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 3;
+    int y = 4;
+
+    printf("x is: %i\n", x);
+    printf("y is: %i\n", y);
+
+    //swapping
+
+    printf("x is now: %i\n", x);
+    printf("y is now: %i\n", y);
+}
